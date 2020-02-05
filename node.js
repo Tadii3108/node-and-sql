2bdb7ec9d04f1b54ee64fc69dca6cb370043a903
Tadii3108 <tadiwa.zingoni@umuzi.org>
2020-02-05 12:46:59 +0200

    added functions

--- node.js
+++ node.js
@@ -21,3 +21,87 @@ const helloWorld = () => {
     );
 };
 helloWorld();
+
+//adds a new visitor to the database table
+const addNewVisitor = () => {
+    pool.query(
+        "INSERT INTO visitors (visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments) values ($1, $2, $3, $4, $5, $6)", ['Tadiwa Zingoni', 20, '01-02-2020', '10:30', 'JoeBoy', 'Fast learner'],
+        (err, info) => {
+            if (err) {
+                throw err
+            }
+            console.log(info.rows)
+        }
+    );
+};
+addNewVisitor();
+
+//returns a list of the name's of visitors and their id's
+const listVisitors = () => {
+    pool.query(
+        "SELECT id, visitor_name FROM visitors",
+        [], (err, info) => {
+            if (err) {
+                throw err
+            }
+            console.log(info.rows)
+        }
+    );
+};
+listVisitors();
+
+//deletes a visitor
+const deleteVisitor = () => {
+    pool.query(
+        "DELETE FROM visitors WHERE $1",
+        [2], (err, info) => {
+            if (err) {
+                throw err
+            }
+            console.log(info.rows)
+        }
+    );
+};
+deleteVisitor();
+
+//updates a visitor
+const updateVisitor = () => {
+    pool.query(
+        "UPDATE visitors SET $1",
+        [3], (err, info) => {
+            if (err) {
+                throw err
+            }
+            console.log(info.rows)
+        }
+    );
+};
+updateVisitor();
+
+//returns all visitor information from id
+const viewVisitor = () => {
+    pool.query(
+        "SELECT * FROM visitors WHERE $1", 
+        [1], (err, info) => {
+            if (err) {
+                throw err
+            }
+            console.log(info.rows)
+        }
+    );
+};
+viewVisitor();
+
+//deletes all entries from visitors table
+const deleteAllVisitors = () => {
+    pool.query(
+        "DELETE * FROM visitors",
+        [], (err, info) => {
+            if (err) {
+                throw err
+            }
+            console.log(info.rows)
+        }
+    );
+};
+deleteAllVisitors();
--- script.sql
+++ script.sql
@@ -1,14 +1,9 @@
-DROP TABLE IF EXISTS "visitors";
-DROP SEQUENCE IF EXISTS visitors_id_seq;
-CREATE SEQUENCE visitors_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
-
-CREATE TABLE "public"."visitors" (
-    "id" integer DEFAULT nextval('visitors_id_seq') NOT NULL,
-    "visitor_name" character varying(50),
-    "visitor_age" integer,
-    "date_of_visit" date,
-    "time_of_visit" time without time zone,
-    "assistant" character varying(50),
-    "comments" character varying(500),
-    CONSTRAINT "visitors_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
+CREATE TABLE visitors (
+    id serial primary key,
+    visitor_name varchar(50),
+    visitor_age int,
+    date_of_visit varchar(10),
+    time_of_visit varchar(5),
+    assistant varchar(50),
+    comments varchar(500),
+)
